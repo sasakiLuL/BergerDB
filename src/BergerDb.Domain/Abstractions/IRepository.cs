8b064b7c94c95f9e2e29db31c806dfbc2b70803e
Sasha Kryvko <sashakrivko75@gmail.com>
2024-12-29 21:26:45 +0100

    Added domain abstractions

---
+++ src/BergerDb.Domain/Abstractions/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using BergerDb.Domain.Primitives.Entities;
+
+namespace BergerDb.Domain.Abstractions;
+
+public interface IRepository<TEntity> where TEntity : Entity
+{
+    void Add(TEntity entity);
+
+    void Delete(TEntity entity);
+}
