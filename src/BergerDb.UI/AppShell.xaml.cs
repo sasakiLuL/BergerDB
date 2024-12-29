3f191b904782d0830872c735874a9e65f5ba343e
Sasha Kryvko <sashakrivko75@gmail.com>
2024-12-29 04:34:30 +0100

    Initial setup of solution

---
+++ src/BergerDb.UI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BergerDb.UI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/BergerDb.UI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BergerDb.UI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
